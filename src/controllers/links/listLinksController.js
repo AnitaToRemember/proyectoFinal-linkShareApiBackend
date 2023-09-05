0e410b7977df9d802cd1623a76853c5426fb5d68
AnitaToRemember <best.anac@gmail.com>
2023-09-05 22:41:53 +0200

    changes from Stefano's comments and adding an updated postman file

--- src/controllers/links/listLinksController.js
+++ src/controllers/links/listLinksController.js
@@ -5,12 +5,12 @@ const selectAllLinkPostsModel = require('../../models/links/selectAllLinkPostsMo
 const listLinksController = async (req, res, next) => {
     try {
         // Obtaining query param.
-        const { keyword } = req.query;
+        const { keyword, sortBy } = req.query;
 
         // Since we want to allow a non-logged in user to access this controller,
          // there will be times when "req.user" does not exist. With the question mark we indicate
          // to JavaScript that "user" can be undefined.
-        const links = await selectAllLinkPostsModel(keyword, req.user?.id);
+        const links = await selectAllLinkPostsModel(keyword, req.user?.id, sortBy);
 
         res.send({
             status: 'ok',
--- src/models/links/selectAllLinkPostsModel.js
+++ src/models/links/selectAllLinkPostsModel.js
@@ -2,11 +2,14 @@
 const getDb = require('../../db/getDb');
 
 // Function that performs a query to the database to obtain the list of links posted.
-const selectAllLinkPostsModel = async (keyword = '', userId = '') => {
+const selectAllLinkPostsModel = async (keyword = '', userId = '',  sortBy = 'date') => {
     let connection;
 
     try {
         connection = await getDb();
+
+        // Determine the ORDER BY clause based on the sortBy parameter
+        const orderByClause = sortBy === 'votes' ? 'ORDER BY votes DESC' : 'ORDER BY L.createdAt DESC';
 
         // Get the list of links posted.
         const [links] = await connection.query(
@@ -24,10 +27,12 @@ const selectAllLinkPostsModel = async (keyword = '', userId = '') => {
             INNER JOIN users U ON U.id = L.userId
             WHERE L.title LIKE ? OR L.url LIKE ? OR L.description LIKE ?
             GROUP BY L.id
-            ORDER BY L.createdAt DESC
+            ${orderByClause}
     `,
     [userId, userId, `%${keyword}%`, `%${keyword}%`, `%${keyword}%`]
         );
+
+            return links;
 
     } finally {
         if (connection) connection.release();
